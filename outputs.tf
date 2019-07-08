c6f9fc1df3e8b0e4d14eca1f98e831186b4aae14
Chris Weyl <cweyl@vindicia.com>
2019-07-08 10:41:11 -0500

    Add `output.ses_smtp_password`
    ...because that's useful.

--- outputs.tf
+++ outputs.tf
@@ -23,3 +23,9 @@ output "secret_access_key" {
   value       = "${join("", aws_iam_access_key.default.*.secret)}"
   description = "The secret access key. This will be written to the state file in plain-text"
 }
+
+output "ses_smtp_password" {
+  sensitive   = true
+  value       = "${join("", aws_iam_access_key.default.*.ses_smtp_password)}"
+  description = "The SES SMTP password. This will be written to the state file in plain-text"
+}
